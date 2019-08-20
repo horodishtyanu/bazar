62f7ed42ec434460dac939bfc32cc8592e5e399e
horodishtyanu <horodishtyanu@gmail.com>
2019-08-20 23:49:35 +0300

    Add test MainController

---
+++ src/Controller/MainControlelr.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainControlelr extends AbstractController
+{
+
+    /**
+     * @Route(path="/test", methods={"GET"})
+    **/
+
+    public function index():Response
+    {
+        return $this->render('base.html.twig', ['param' => 'asdasd']);
+    }
+}
